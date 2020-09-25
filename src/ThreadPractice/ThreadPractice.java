50e2bfd89f82c8f3bbef7bdf115502d5e870c619
mikeremerscheid <mikeremerscheid@gmail.com>
2020-09-25 08:55:38 -0400

    fourth commit

--- src/ThreadPractice/ThreadPractice.java
+++ src/ThreadPractice/ThreadPractice.java
@@ -3,7 +3,7 @@ package ThreadPractice;
 public class ThreadPractice {
 
     public static void main(String[] args) {
-
+        System.out.println("ThreadPractice");
         var t1 = new Thread();
         t1.start();
 
